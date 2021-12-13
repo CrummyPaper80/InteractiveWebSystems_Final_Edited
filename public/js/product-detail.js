2e702b008126699335c72fade37c193d22320f45
Johnathon Brunette <jbgamming2002@gmail.com>
2021-12-13 13:55:58 -0500

    added more js and images and updated pages

--- public/js/product-detail.js
+++ public/js/product-detail.js
@@ -13,47 +13,33 @@ document.querySelector('#view-all-description').addEventListener('click', () => 
 
 let products = [
     {
-        name: 'JBL E55BT KEY BLACK',
-        image1: './images/JBL_E55BT_KEY_BLACK_6175_FS_x1-1605x1605px.png',
-        image2: './images/JBL_LIVE650BTNC_Product Image_Folded_Black.webp',
-        old_price: '400',
-        curr_price: '300'
+        name: 'AMD Prebuilt 1',
+        tag: "amdprebuilt1",
+        prev_price: "600",
+        curr_price: "500",
+        inCart: 0
     },
     {
-        name: 'JBL JR 310BT',
-        image1: './images/JBL_JR 310BT_Product Image_Hero_Skyblue.png',
-        image2: './images/JBL_JR 310BT_Product Image_Detail_Skyblue.png',
-        old_price: '400',
-        curr_price: '300'
+        name: 'AMD Prebuilt 2',
+        tag: "amdprebuilt2",
+        prev_price: '1000',
+        curr_price: '900',
+        inCart: 0
     },
     {
-        name: 'JBL TUNE 750BTNC',
-        image1: './images/kisspng-beats-electronics-headphones-apple-beats-studio-red-headphones.png',
-        image2: './images/JBL_E55BT_KEY_RED_6063_FS_x1-1605x1605px.webp',
-        old_price: '400',
-        curr_price: '300'
+        name: 'Intel Prebuilt 1',
+        tag: "intelprebuilt1",
+        prev_price: "600",
+        curr_price: "500",
+        inCart: 0
     },
     {
-        name: 'JBL Horizon',
-        image1: './images/JBLHorizon_001_dvHAMaster.png',
-        image2: './images/JBLHorizon_BLK_002_dvHAMaster.webp',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'JBL Tune 220TWS',
-        image1: './images/JBL_TUNE220TWS_ProductImage_Pink_ChargingCaseOpen.png',
-        image2: './images/JBL_TUNE220TWS_ProductImage_Pink_Back.png',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'UA Project Rock',
-        image1: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_HERO.png',
-        image2: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_BACK.png',
-        old_price: '400',
-        curr_price: '300'
-    },
+        name: 'Intel Prebuilt 2',
+        tag: "intelprebuilt2",
+        prev_price: '1000',
+        curr_price: '900',
+        inCart: 0
+    }
 ]
 
 let product_list = document.querySelector('#related-products')
